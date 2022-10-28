9b62cf4ba630315a2272664e932a243cc1f61853
Paul Holmberg <paul.holmberg@elev.ga.lbs.se>
2022-10-28 17:39:05 +0200

    sss

--- include/rsa_selection.c
+++ include/rsa_selection.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <gmp.h>
+#include <string.h>
 #include "irandom.h"
+#include "fileop.h"
+#include <openssl/sha.h>
+#define MAX_KEY_LENGTH 4096
 
 void rsa_profile(mp_bitcnt_t bit_size) {
 	// Generate RSA profile and output results to file.
@@ -27,19 +31,64 @@ void rsa_profile(mp_bitcnt_t bit_size) {
 	// Create file streams
 	FILE* private_fptr = fopen("private_key", "w+");
 	FILE* public_fptr = fopen("public_key", "w+");
-	gmp_fprintf(private_fptr, "%Zd\n%Zd\n", o, d);
+	gmp_fprintf(private_fptr, "%Zx\n%Zx\n", o, d);
 	fclose(private_fptr);
-	gmp_fprintf(public_fptr, "%Zd", n); // Key is automatically assumed to be 65537
+	gmp_fprintf(public_fptr, "%Zx", n); // Key is automatically assumed to be 65537
 	fclose(public_fptr);
 }
 
-void read_profile() {
-
+int rsa_sign_message(char message[], char key_location[]) {
+	// Takes a < bit_size message, hashes its contents, and signs the hash.
+	mpz_t o, d;
+	FILE* key_fptr = fopen(key_location, "r");
+	if (key_fptr == NULL) {
+		perror("Could not read from file");
+		return (-1);
+	}
+	char phi_value[MAX_KEY_LENGTH], private_exponent[MAX_KEY_LENGTH];
+	fscanf(key_fptr, "%s %s", phi_value, private_exponent);
+	mpz_init_set_str(o, phi_value, 16);
+	mpz_init_set_str(d, private_exponent, 16);
+	gmp_printf("%Zd\n%Zd\n", o, d);
+	return 0;
 }
 
+int rsa_add_profile(char path_to_file[], char fingerprint[], char profile_name[]) {
+	// Reads a public key from a file or string.
+	FILE* path_ptr = fopen(path_to_file, "r");
+	if (path_ptr == NULL && fingerprint != NULL) {
+		// Recycle code down below
+	}
+	else if (path_ptr == NULL && fingerprint == NULL) {
+		perror("Could not read public key profile");
+		return (-1);
+	}
+	char public_key[MAX_KEY_LENGTH]; // Read public key from file and put it in a string.
+	fscanf(path_ptr, "%s", public_key);
+	fclose(path_ptr);
 
+	FILE* keyring = fopen("/usr/.database", "a");
+	if (keyring == NULL) {
+		perror("Could not access keyring file");
+		return (-1);
+	}
+	
+	if (read_filec(public_key) < 1) {
+		printf("Public key already exists in keyring.");
+		return 0;
+	}
+	
+	fputs(profile_name, keyring);
+	fputs("\t", keyring);
+	fputs(public_key, keyring);
+	fputs("\n", keyring);
 
-void main() {
+	fclose(keyring);
+	return 1;
+}
+
+int main() {
 	mp_bitcnt_t bit = 4096;
-	rsa_profile(bit);
+	rsa_add_profile("public_key", NULL, "John");
+return 0;
 }
